29b253f862aa68ea4e5f756d76aed95d018ecb4c
SanzharShadybekov <shadubekovs777@gmail.com>
2023-03-22 10:59:20 +0600

    initial commit

---
+++ like/views.py
@@ -0,0 +1,29 @@
+from rest_framework import generics, permissions
+
+from like import serializers
+from like.models import Like
+from post.permissons import IsAuthor
+
+
+class LikeCreateView(generics.CreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = serializers.LikeSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeDeleteView(generics.DestroyAPIView):
+    queryset = Like.objects.all()
+    permission_classes = (permissions.IsAuthenticated,
+                          IsAuthor)
+
+
+
+
+
+
+
+
+
+
